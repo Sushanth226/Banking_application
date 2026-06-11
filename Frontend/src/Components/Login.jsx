b11afa96fc53eb6bfe3fa0a651757cc336149bd3
Sushanth226 <surusourya@gmail.com>
2026-06-11 22:38:34 +0530

    feat: add axios for API calls and implement login functionality in Login component

--- Frontend/src/Components/Login.jsx
+++ Frontend/src/Components/Login.jsx
@@ -1,7 +1,44 @@
+import {useState} from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 function Login(){
+   const navigate=useNavigate();
+   const [name,setname]=useState("");
+   const [password,setpassword]=useState("");
+   const [email,setemail]=useState("");
+   const nameChange=(e)=>{
+      setname((name)=>e.target.value);
+   }
+   const passwordChange=(e)=>{
+      setpassword((password)=>e.target.value);
+   }
+   const emailChange=(e)=>{
+      setemail((email)=>e.target.value);
+   }
+   const goToDashBoard=async()=>{
+       try{
+          const result=await axios.post("http://localhost:5000/auth/login",{email,password},{ withCredentials:true});
+          navigate("/dashboard");
+       }catch(error){
+         console.log(error);
+         alert(error);
+       }
+   }
    return(
     <>
+    <header>
     <h1>Login Page</h1>
+    </header>
+    <main>
+      <section>
+    <form onSubmit={goToDashBoard}>
+      <input type="text" onChange={nameChange} placeholder="Give name"/>
+      <input type="text" onChange={emailChange} placeholder="Give email"/>
+      <input type="text" onChange={passwordChange} placeholder="Give password"/>
+      <button type="submit" >Submit</button>
+    </form>
+    </section>
+    </main>
     </>
    )
 }
