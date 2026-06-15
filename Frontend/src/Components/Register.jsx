347e6992d86ad1ae9481ed0be78a3f232f16deb1
Sushanth226 <surusourya@gmail.com>
2026-06-15 10:23:53 +0530

    feat: implement registration functionality in Register component

--- Frontend/src/Components/Register.jsx
+++ Frontend/src/Components/Register.jsx
@@ -1,7 +1,44 @@
+import {useState} from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 function Register(){
-  return(
+   const navigate=useNavigate();
+   const [name,setname]=useState("");
+   const [password,setpassword]=useState("");
+   const [email,setemail]=useState("");
+   const nameChange=(e)=>{
+      setname((name)=>e.target.value);
+   }
+   const passwordChange=(e)=>{
+      setpassword((password)=>e.target.value);
+   }
+   const emailChange=(e)=>{
+      setemail((email)=>e.target.value);
+   }
+   const goToDashBoard=async()=>{
+       try{
+          const result=await axios.post("http://localhost:5000/auth/register",{name,email,password},{ withCredentials:true});
+          navigate("/dashboard");
+       }catch(error){
+         console.log(error);
+         alert(error);
+       }
+   }
+   return(
     <>
-    <h1>Registeration Page</h1>
+    <header>
+    <h1>Register Page</h1>
+    </header>
+    <main>
+      <section>
+    <form onSubmit={goToDashBoard}>
+      <input type="text" onChange={nameChange} placeholder="Give name"/>
+      <input type="text" onChange={emailChange} placeholder="Give email"/>
+      <input type="text" onChange={passwordChange} placeholder="Give password"/>
+      <button type="submit" >Submit</button>
+    </form>
+    </section>
+    </main>
     </>
    )
 }
